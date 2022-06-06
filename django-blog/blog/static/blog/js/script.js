dacf05563a9bd756492c9e12785bfea089b6888a
JuneHoo <3331562927@qq.com>
2022-06-06 16:34:36 -0700

    final

--- django-blog/blog/static/blog/js/script.js
+++ django-blog/blog/static/blog/js/script.js
@@ -28,16 +28,6 @@ $("#search-menu").click(function(e){
     }
 });
 
-/*!
- * classie - class helper functions
- * from bonzo https://github.com/ded/bonzo
- * 
- * classie.has( elem, 'my-class' ) -> true/false
- * classie.add( elem, 'my-new-class' )
- * classie.remove( elem, 'my-unwanted-class' )
- * classie.toggle( elem, 'my-class' )
- */
-
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
@@ -51,8 +41,6 @@ function classReg( className ) {
   return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
 }
 
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
 var hasClass, addClass, removeClass;
 
 if ( 'classList' in document.documentElement ) {
